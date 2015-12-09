b66e38adc6f99570bb72e4f8157cd0f9429e6ce0
Craig P Jolicoeur <cpjolicoeur@gmail.com>
2015-12-09 16:58:11 -0500

    Better walking of SVG file with <polygon> and <path> nodes

--- pollyanna.go
+++ pollyanna.go
@@ -4,13 +4,28 @@ import (
 	"encoding/xml"
 	"errors"
 	"fmt"
+	"regexp"
 	"strings"
 )
 
 var baseDOM = `<div class="pollyanna"><div class="wrap">%s</div></div>`
 var shardDOM = `<div class="shard-wrap"><div class="shard"></div></div>`
-var baseCSS = `.pollyanna{position:absolute;width:100%;height:100%;top:0;left:0}.wrap{width:800;height:600px;top:5%;left:5%;position:absolute}.shard-wrap,.shard-wrap .shard,.shard-wrap .shard::before{width:100%;height:100%;position:absolute}.shard-wrap{z-index:2}.shard-wrap .shard{background-color:#fff}.shard-wrap .shard::before{content:"";background:rgba(255,255,255,0);top:0;left:0}%s`
-var shardCSS = `.shard-wrap:nth-child(%d) .shard{-webkit-clip-path:%s;background-color:%s;}`
+
+var baseCSS = `.pollyanna{position:absolute;width:100%;height:100%;top:0;left:0}.wrap{width:800;height:600px;top:5%;left:5%;position:absolute}.shard-wrap,.shard-wrap .shard,.shard-wrap .shard::before{width:100%;height:100%;position:absolute}.shard-wrap{z-index:2}.shard-wrap .shard{background-color:#fff}.shard-wrap .shard::before{content:"";background:rgba(255,255,255,0);top:0;left:0}`
+var shardCSS = `.shard-wrap:nth-child(%d) .shard{-webkit-clip-path:%s;clip-path:%s;background-color:%s;}`
+
+// Node is a generic SVG XML node
+type Node struct {
+	XMLName   xml.Name
+	Nodes     []Node `xml:",any"`
+	Width     string `xml:"width,attr"`
+	Height    string `xml:"height,attr"`
+	ViewBox   string `xml:"viewBox,attr"`
+	Fill      string `xml:"fill,attr"`
+	RawPoints string `xml:"points,attr"`
+	D         string `xml:"d,attr"`
+	ID        string `xml:"id,attr"`
+}
 
 // Svg is the top level <svg/> node
 type Svg struct {
@@ -26,6 +41,12 @@ type Svg struct {
 type Polygon struct {
 	Fill      string `xml:"fill,attr"`
 	RawPoints string `xml:"points,attr"`
+}
+
+// Path is a single <path/> node
+type Path struct {
+	Fill    string `xml:"fill,attr"`
+	RawPath string `xml:"d,attr"`
 }
 
 // Output represents the associated HTML Dom and CSS markup for the SVG image
@@ -46,18 +67,41 @@ type cssRule struct {
 
 // ParseSVG will parse the incoming SVG document bytes
 func ParseSVG(bytes []byte) (Svg, error) {
+	var n Node
 	var svg Svg
 
-	err := xml.Unmarshal(bytes, &svg)
+	err := xml.Unmarshal(bytes, &n)
 	if err != nil {
 		return svg, err
 	}
 
+	if "svg" != n.XMLName.Local {
+		return svg, errors.New("No root-level <svg> node found in the input data.")
+	}
+
+	svg.Polygons = n.BuildPolygons()
 	if 0 == len(svg.Polygons) {
-		return svg, errors.New("No <polygon/> nodes were found in the SVG data.")
+		return svg, errors.New("No <polygon/> or <path/> nodes were found in the SVG data.")
 	}
 
 	return svg, nil
+}
+
+func (node Node) BuildPolygons() []Polygon {
+	polygons := []Polygon{}
+
+	walk([]Node{node}, func(n Node) bool {
+		switch n.XMLName.Local {
+		case "polygon":
+			polygons = append(polygons, Polygon{n.Fill, n.RawPoints})
+		case "path":
+			p := Path{n.Fill, n.D}
+			polygons = append(polygons, p.ToPolygon())
+		}
+		return true
+	})
+
+	return polygons
 }
 
 // GenerateOutput converts the raw SVG data into HTML DOM nodes and associated CSS rules
@@ -72,25 +116,47 @@ func (s Svg) cssShardChildren() string {
 	css := make([]string, len(s.Polygons))
 
 	for i, p := range s.Polygons {
-		css[i] = fmt.Sprintf(shardCSS, i+1, p.FormattedCSSPolygonPoints(), p.Fill)
+		cssPolyString := p.FormattedCSSPolygonPoints()
+		css[i] = fmt.Sprintf(shardCSS, i+1, cssPolyString, cssPolyString, p.Fill)
 	}
 
 	return strings.Join(css, ``)
 }
 
 func (p Polygon) FormattedCSSPolygonPoints() string {
-	return fmt.Sprintf("polygon(%s)", cssPolygonBuilder(p.Points()[0], p.Points()[1:len(p.Points())]))
+	points := p.Points()
+	return fmt.Sprintf("polygon(%s)", cssPolygonBuilder(points[0], points[1:len(points)]))
 }
 
 func (p Polygon) Points() [][]string {
 	var points [][]string
-	pointsStr := strings.Split(strings.Trim(p.RawPoints, ` `), ` `)
 
-	for _, pointStr := range pointsStr {
-		points = append(points, strings.Split(pointStr, `,`))
+	re := regexp.MustCompile(`(?:\d+(?:\.\d+)?[, ]\d+(?:\.\d+)?)`)
+	coords := re.FindAllString(strings.Trim(p.RawPoints, ` `), -1)
+
+	for _, coord := range coords {
+		// Normalize the coord pair to be comma separated, then split on that comma
+		points = append(points, strings.Split(strings.Replace(coord, ` `, `,`, 1), `,`))
 	}
 
 	return points
+}
+
+// ToPolygon converts a <path> node to a Polygon object
+func (p Path) ToPolygon() Polygon {
+	var polygon Polygon
+
+	polygon.Fill = p.Fill
+
+	re := regexp.MustCompile(`(?:\d+(?:\.?\d+?))`)
+	coords := re.FindAllString(p.RawPath, -1)
+	polygon.RawPoints = strings.Join(coords, ` `)
+
+	return polygon
+}
+
+func (n Node) String() string {
+	return fmt.Sprintf("Node: ID - %s, Fill - %s", n.ID, n.Fill)
 }
 
 func (s Svg) String() string {
@@ -120,4 +186,12 @@ func cssPolygonBuilder(points []string, rest [][]string) string {
 
 func cssFormatSinglePoint(p []string) string {
 	return fmt.Sprintf("%spx %spx", p[0], p[1])
+}
+
+func walk(nodes []Node, f func(Node) bool) {
+	for _, n := range nodes {
+		if f(n) {
+			walk(n.Nodes, f)
+		}
+	}
 }
--- pollyanna_test.go
+++ pollyanna_test.go
@@ -17,7 +17,15 @@ func TestParsing(t *testing.T) {
 			So(err.Error(), ShouldEqual, "EOF")
 		})
 
-		Convey("Handles SVG files that containing non-polygon nodes", func() {
+		Convey("Handles non-svg xml data", func() {
+			svgData := []byte(`<parent><child1>foo</child1><child2 /></parent>`)
+
+			_, err := ParseSVG(svgData)
+			So(err, ShouldNotBeNil)
+			So(err.Error(), ShouldEqual, "No root-level <svg> node found in the input data.")
+		})
+
+		Convey("Handles SVG files that containing non-polygon or path nodes", func() {
 			svgData := []byte(`
 			<svg viewBox='0 0 125 80' xmlns='http://www.w3.org/2000/svg'>
 				<text y="75" font-size="100" font-family="serif"><![CDATA[10]]></text>
@@ -27,10 +35,10 @@ func TestParsing(t *testing.T) {
 			svg, err := ParseSVG(svgData)
 			So(svg.Polygons, ShouldHaveLength, 0)
 			So(err, ShouldNotBeNil)
-			So(err.Error(), ShouldEqual, `No <polygon/> nodes were found in the SVG data.`)
+			So(err.Error(), ShouldEqual, `No <polygon/> or <path/> nodes were found in the SVG data.`)
 		})
 
-		Convey("Handles SVG files with no <polygon/> nodes", func() {
+		Convey("Handles SVG files with no <polygon/> or <path/> nodes", func() {
 			svgData := []byte(`<?xml version="1.0" encoding="utf-8"?>
 			<svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="800px" height="600px" viewBox="0 0 800 600" enable-background="new 0 0 800 600" xml:space="preserve">
 			</svg>`)
@@ -46,7 +54,7 @@ func TestParsing(t *testing.T) {
 		<svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="800px" height="600px" viewBox="0 0 800 600" enable-background="new 0 0 800 600" xml:space="preserve">
 		<polygon fill="#F3CD5E" points="366.4,7.6 432.7,5.6 430.6,67.7 401.1,71.1 "/>
 		<polygon fill="#F1BD36" points="432.7,5.6 441.3,66.2 401.1,71.1 "/>
-		<polygon fill="#B77E00" points="366.4,7.6 364.1,67.7 401.1,71.1 "/>
+		<polygon fill="#B77E00" points="366.4 7.6 364.1 67.7 401.1 71.1 "/>
 		</svg>`)
 
 		Convey("Properly parses all <polygon/> nodes", func() {
@@ -74,18 +82,43 @@ func TestParsing(t *testing.T) {
 			So(svg.Polygons[0].Points(), ShouldHaveLength, 4)
 			So(svg.Polygons[0].Points(), ShouldResemble, expectedPoints)
 		})
+
+		Convey("Handles SVG files with only <path> nodes", func() {
+			svgData := []byte(`<?xml version="1.0" encoding="utf-8"?>
+			<svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="800px" height="600px" viewBox="0 0 800 600" enable-background="new 0 0 800 600" xml:space="preserve">
+				<path fill="#F1BD36" d="M432.7,5.6 L441.3,66.2 L401.1,71.1 "/>
+				<path fill="#F1BD36" d="M 432.7 5.6 L 441.3 66.2 L 401.1 71.1 "/>
+				<path fill="#F1BD36" d="M 432.7 5.6 L 441.3 66.2 401.1 71.1 "/>
+			</svg>`)
+
+			svg, err := ParseSVG(svgData)
+			So(svg.Polygons, ShouldHaveLength, 3)
+			So(err, ShouldBeNil)
+		})
+
+		Convey("Handles SVG files with both <polygon> and <path> nodes", func() {
+			svgData := []byte(`<?xml version="1.0" encoding="utf-8"?>
+			<svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="800px" height="600px" viewBox="0 0 800 600" enable-background="new 0 0 800 600" xml:space="preserve">
+				<polygon fill="#F1BD36" points="432.7,5.6 441.3,66.2 401.1,71.1 "/>
+				<path fill="#F1BD36" d="M 432.7 5.6 L 441.3 66.2 L 401.1 71.1 "/>
+			</svg>`)
+
+			svg, err := ParseSVG(svgData)
+			So(svg.Polygons, ShouldHaveLength, 2)
+			So(err, ShouldBeNil)
+		})
 	})
 }
 
 func TestGenerateOutput(t *testing.T) {
 	Convey("Invalid SVG Data:", t, nil)
 
-	Convey("Valid SVG Data:", t, func() {
+	Convey("Valid SVG Polygon Data:", t, func() {
 		svgData := []byte(`<?xml version="1.0" encoding="utf-8"?>
 		<svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="800px" height="600px" viewBox="0 0 800 600" enable-background="new 0 0 800 600" xml:space="preserve">
-		<polygon fill="#F3CD5E" points="366.4,7.6 432.7,5.6 430.6,67.7 401.1,71.1 "/>
-		<polygon fill="#F1BD36" points="432.7,5.6 441.3,66.2 401.1,71.1 "/>
-		<polygon fill="#B77E00" points="366.4,7.6 364.1,67.7 401.1,71.1 "/>
+			<polygon fill="#F3CD5E" points="366.4,7.6 432.7,5.6 430.6,67.7 401.1,71.1 "/>
+			<polygon fill="#F1BD36" points="432.7,5.6 441.3,66.2 401.1,71.1 "/>
+			<polygon fill="#B77E00" points="366.4 7.6 364.1 67.7 401.1 71.1 "/>
 		</svg>`)
 		svg, _ := ParseSVG(svgData)
 		output, _ := svg.GenerateOutput()
@@ -102,7 +135,7 @@ func TestGenerateOutput(t *testing.T) {
 		})
 
 		Convey("CSS rules", func() {
-			Convey("Builds one nth-child shard-wrap rule per Pollygon", func() {
+			Convey("Builds one nth-child shard-wrap rule per Polygon", func() {
 				So(strings.Count(output.CSS, "nth-child"), ShouldEqual, len(svg.Polygons))
 			})
 
@@ -117,6 +150,49 @@ func TestGenerateOutput(t *testing.T) {
 				So(output.CSS, ShouldContainSubstring, "polygon(432.7px 5.6px, 441.3px 66.2px, 401.1px 71.1px)")
 				So(output.CSS, ShouldContainSubstring, "polygon(366.4px 7.6px, 364.1px 67.7px, 401.1px 71.1px)")
 			})
+		})
+	})
+
+	Convey("Valid SVG Path Data:", t, func() {
+		svgData := []byte(`<?xml version="1.0" encoding="utf-8"?>
+		<svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="800px" height="600px" viewBox="0 0 800 600" enable-background="new 0 0 800 600" xml:space="preserve">
+			<path fill="#F1BD36" d="M432.7,5.6 L441.3,66.2 L401.1,71.1 "/>
+			<path fill="#F1BD36" d="M 32.7 15.6 L 44.3 6.2 L 1.1 71.1 "/>
+			<path fill="#F1BD36" d="M 42.7 8.6 L 41.0 66 40.1 71.1 "/>
+		</svg>`)
+		svg, _ := ParseSVG(svgData)
+		output, _ := svg.GenerateOutput()
+
+		Convey("Converts all path nodes to Polygons", func() {
+			So(len(svg.Polygons), ShouldEqual, 3)
+		})
+
+		Convey("Builds one shard per Polygon", func() {
+			So(strings.Count(output.HTML, "shard-wrap"), ShouldEqual, len(svg.Polygons))
+		})
+
+		Convey("Builds the right points per Polygon", func() {
+			So(output.CSS, ShouldContainSubstring, "polygon(432.7px 5.6px, 441.3px 66.2px, 401.1px 71.1px)")
+			So(output.CSS, ShouldContainSubstring, "polygon(32.7px 15.6px, 44.3px 6.2px, 1.1px 71.1px)")
+			So(output.CSS, ShouldContainSubstring, "polygon(42.7px 8.6px, 41.0px 66px, 40.1px 71.1px)")
+		})
+	})
+
+	Convey("Valid SVG Polygon & Path Data:", t, func() {
+		svgData := []byte(`<?xml version="1.0" encoding="utf-8"?>
+		<svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="800px" height="600px" viewBox="0 0 800 600" enable-background="new 0 0 800 600" xml:space="preserve">
+			<polygon fill="#F3CD5E" points="366.4,7.6 432.7,5.6 430.6,67.7 401.1,71.1 "/>
+			<path fill="#F1BD36" d="M 432.7 5.6 L 441.3 66.2 401.1 71.1 "/>
+		</svg>`)
+		svg, _ := ParseSVG(svgData)
+		output, _ := svg.GenerateOutput()
+
+		Convey("Converts all polygon and path nodes to Polygons", func() {
+			So(len(svg.Polygons), ShouldEqual, 2)
+		})
+
+		Convey("Builds one shard per Polygon", func() {
+			So(strings.Count(output.HTML, "shard-wrap"), ShouldEqual, len(svg.Polygons))
 		})
 	})
 }
